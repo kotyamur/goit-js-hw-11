2d51e53eb1e8ca34c5b413d6e553826d21bca875
Kateryna Kleshch <ketrins1993@gmail.com>
2022-09-10 20:54:33 +0300

    do task with fetch

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -6,14 +6,12 @@ const BASE_URL = 'https://pixabay.com/api/';
 //   },
 // };
 
-const parameters = '';
-export const fetchImages = (inputValue, p) => {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${p}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-      }
-    return response.json();
-  });
+export const fetchImages = (inputValue, p, perPage) => {
+    const url = `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${p}`;
+    return fetch(url).then(response => {
+        if (!response.ok) {
+        throw new Error(response.status);
+        }
+        return response.json();
+    });
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/common.css';
 import './css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './fetchImages.js'
-
-const DEBOUNCE_DELAY = 300;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const searchFormEl = document.querySelector('.search-form');
 const picturesContainerEl = document.querySelector('.gallery');
@@ -10,81 +11,96 @@ const loadMoreBtnEl = document.querySelector('.load-more');
 
 let searchQuery = '';
 let page = 1;
+const perPage = 40;
+let simpleLightboxGallery = new SimpleLightbox('.gallery a');
+
+const appendPicturesMarkup = arr => {
+     const markup = arr
+       .map(image => {
+            return `
+                <div class="photo-card">
+                    <a class="gallery-link" href="${image.largeImageURL}">
+                        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="gallery-image"/>
+                    </a>
+                    <div class="info">
+                        <p class="info-item">
+                        <b>Likes</b>${image.likes}
+                        </p>
+                        <p class="info-item">
+                        <b>Views</b>${image.views}
+                        </p>
+                        <p class="info-item">
+                        <b>Comments</b>${image.comments}
+                        </p>
+                        <p class="info-item">
+                        <b>Downloads</b>${image.downloads}
+                        </p>
+                    </div>
+                </div>
+            `;
+       })
+       .join('');
+     picturesContainerEl.insertAdjacentHTML('beforeend', markup);
+}
+
+const clearPicturesContainer = () => {
+    picturesContainerEl.innerHTML = '';
+}
 
 const onSearchBtn = (evt) => {
     evt.preventDefault();
-    console.log(evt.currentTarget.elements.searchQuery.value);
     searchQuery = evt.currentTarget.elements.searchQuery.value;
+    if (searchQuery === '') {
+        Notify.failure('Please enter your search query.');
+        return
+    }
     page = 1;
+    clearPicturesContainer();
 
-    fetchImages(searchQuery, page).then(data => {
-        console.log(data)
-        const arr = data.hits;
-        const markup = arr
-          .map(image => {
-            return `
-                <div class="photo-card">
-                    <div class="gallery-link">
-                        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="gallery-image"/>
-                    </div>
-                    <div class="info">
-                        <p class="info-item">
-                        <b>Likes</b>${image.likes}
-                        </p>
-                        <p class="info-item">
-                        <b>Views</b>${image.views}
-                        </p>
-                        <p class="info-item">
-                        <b>Comments</b>${image.comments}
-                        </p>
-                        <p class="info-item">
-                        <b>Downloads</b>${image.downloads}
-                        </p>
-                    </div>
-                </div>
-              `;
-          })
-          .join('');
-        picturesContainerEl.insertAdjacentHTML('beforeend', markup);
+    loadMoreBtnEl.classList.add('is-hidden');
+    fetchImages(searchQuery, page, perPage).then(data => {
+        if (data.totalHits === 0) {
+            Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+            );
+            return;
+        }
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        appendPicturesMarkup(data.hits);
+        simpleLightboxGallery.refresh();
         page += 1;
+        loadMoreBtnEl.classList.remove('is-hidden');
+        if (data.totalHits <= perPage) {
+            setTimeout(() => {
+              Notify.info('There are all images matching your search query.');
+              loadMoreBtnEl.classList.add('is-hidden');
+              return;
+            }, 1000);
+        };
     });
+    
 };
 
 const onLoadMoreBtn = (evt) => {
-    fetchImages(searchQuery, page).then(data => {
-        console.log(data.hits);
-        const arr = data.hits;
-        const markup = arr
-          .map(image => {
-              return `
-                <div class="photo-card">
-                    <div class="gallery-link">
-                        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="gallery-image"/>
-                    </div>
-                    <div class="info">
-                        <p class="info-item">
-                        <b>Likes</b>${image.likes}
-                        </p>
-                        <p class="info-item">
-                        <b>Views</b>${image.views}
-                        </p>
-                        <p class="info-item">
-                        <b>Comments</b>${image.comments}
-                        </p>
-                        <p class="info-item">
-                        <b>Downloads</b>${image.downloads}
-                        </p>
-                    </div>
-                </div>
-              `;
-          })
-            .join('');
-        picturesContainerEl.insertAdjacentHTML('beforeend', markup);
-      page += 1;
+    fetchImages(searchQuery, page, perPage).then(data => {
+        const countOfPages = data.totalHits / perPage;
+        if (page >= countOfPages) {
+          Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+          loadMoreBtnEl.classList.add('is-hidden');
+          return;
+        }
+        appendPicturesMarkup(data.hits);
+        simpleLightboxGallery.refresh();
+      
+        page += 1;
     });
 }
 
 searchFormEl.addEventListener('submit', onSearchBtn);
 loadMoreBtnEl.addEventListener('click', onLoadMoreBtn);
+
+
 
 
